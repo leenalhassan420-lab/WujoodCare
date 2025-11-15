4a82bcbd58becd386b459479a9bf69d9f8ec4b5e
raed-sh <raedsh.work@gmail.com>
2025-11-15 16:45:19 +0300

    fix: config path for production

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: isProd ? "/WujoodCare" : "", // TODO: this should follow the domain
-  assetPrefix: isProd ? "/WujoodCare" : "",
+  // basePath: isProd ? "/" : "", // TODO: this should follow the domain
+  // assetPrefix: isProd ? "/" : "",
   images: {
     unoptimized: true,
   },
